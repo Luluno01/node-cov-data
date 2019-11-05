ebe0aee0c22072f1d47c636dc590291b5997ae22
Untitled <littleluluno01@163.com>
2019-11-05 15:01:53 +0800

    minor changes in types

--- src/Importer.ts
+++ src/Importer.ts
@@ -2,8 +2,8 @@ import { EventBarrier } from 'unlib.js/build/Sync/EventBarrier'
 import { CoverageData } from './CoverageData'
 
 
-export interface CoverageAndResult {
-  coverage: CoverageData
+export interface CoverageAndResult<T={}> {
+  coverage: CoverageData<T>
   passed: boolean
 }
 
@@ -15,7 +15,7 @@ export class Importer extends EventBarrier {
    * Import coverage data and test result from somewhere
    * @param pathPrefix
    */
-  public async *import(pathPrefix: string): AsyncGenerator<CoverageAndResult> {
+  public async *import<T={}>(pathPrefix: string): AsyncGenerator<CoverageAndResult<Partial<T>>> {
     throw new Error('Not implemented')
   }
 }
