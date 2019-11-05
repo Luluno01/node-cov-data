829745c6fdbd68fc2a78d2ede6f335bb859b1498
Untitled <littleluluno01@163.com>
2019-11-05 16:20:03 +0800

    update `Importer`

--- src/Importer.ts
+++ src/Importer.ts
@@ -1,5 +1,5 @@
-import { EventBarrier } from 'unlib.js/build/Sync/EventBarrier'
 import { CoverageData } from './CoverageData'
+import { EventEmitter } from 'events'
 
 
 export interface CoverageAndResult<T={}> {
@@ -7,15 +7,40 @@ export interface CoverageAndResult<T={}> {
   passed: boolean
 }
 
+export interface Importer<T={}> extends EventEmitter {
+  emit(event: 'data', data: CoverageAndResult<Partial<T>>): boolean
+  emit(event: 'end'): boolean
+
+  on(event: 'data', handler: (data: CoverageAndResult<Partial<T>>) => void): this
+  on(event: 'end', handler: () => void): this
+
+  once(event: 'data', handler: (data: CoverageAndResult<Partial<T>>) => void): this
+  once(event: 'end', handler: () => void): this
+
+  off(event: 'data', handler: (data: CoverageAndResult<Partial<T>>) => void): this
+  off(event: 'end', handler: () => void): this
+}
+
 /**
  * Base class for all importers
  */
-export class Importer extends EventBarrier {
+export class Importer<T={}> extends EventEmitter {
   /**
    * Import coverage data and test result from somewhere
    * @param pathPrefix
    */
-  public async *import<T={}>(pathPrefix: string): AsyncGenerator<CoverageAndResult<Partial<T>>> {
+  public async *importAsIterator(pathPrefix: string): AsyncGenerator<CoverageAndResult<Partial<T>>> {
+    throw new Error('Not implemented')
+  }
+
+  /**
+   * Import coverage data and test result from somewhere
+   * 
+   * Note that coverage data and result will be delivered by `data` event, and
+   * a `end` event will be emitted when the import process is done
+   * @param pathPrefix
+   */
+  public import(pathPrefix: string): void {
     throw new Error('Not implemented')
   }
 }
